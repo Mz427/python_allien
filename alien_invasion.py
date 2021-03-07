3f24b41868601c1e83bfb5a0ab868d8995a6257b
Ma Zhuang <mz@MzHost.my.domain>
2021-03-07 17:44:04 +0800

    new file:   alien_alien.py 	modified:   alien_bullet.py 	modified:   alien_invasion.py 	modified:   alien_settings.py 	modified:   alien_ship.py 	new file:   image_alien.bmp 	modified:   image_ship2.bmp

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from alien_settings import Settings
 from alien_ship import Ship
 from alien_bullet import Bullet
+from alien_alien import Alien
 
 class AlienInvasion:
     def __init__(self):
@@ -12,6 +13,7 @@ class AlienInvasion:
                 (self.settings.screen_width, self.settings.screen_heigth))
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
         pygame.display.set_caption("Alien Invasion")
 
     def _check_keydown_events(self, event):
@@ -39,22 +41,70 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
+    def _create_alien(self, alien_number, row_number):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        avaliable_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = avaliable_space_x // (2 * alien_width)
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_heigth - (3 * alien_height) - ship_height)
+        number_row = available_space_y // (2 * alien_height)
+
+        for row_number in range(number_row):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+        
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _update_screen(self):
         self.screen.fill(self.settings.background_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+
         pygame.display.flip()
 
     def run_game(self):
+        self._create_fleet()
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 if __name__ == "__main__":
--- alien_settings.py
+++ alien_settings.py
@@ -1,7 +1,7 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 800
-        self.screen_heigth = 600
+        self.screen_width = 1230
+        self.screen_heigth = 900
         self.background_color = (143, 188, 143)
 
         self.ship_speed = 1.5
@@ -9,4 +9,9 @@ class Settings:
         self.bullet_speed = 0.5
         self.bullet_width = 10
         self.bullet_heigth = 15
+        self.bullet_allowed = 3
         self.bullet_color = (60, 60, 60)
+
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
